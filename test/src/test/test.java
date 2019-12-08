61b89bdd15a438851497c92db23e96dd819b85b1
suxudong <2315399136@qq.com>
2019-12-08 15:23:58 +0800

    提交项目到git

---
+++ test/src/test/test.java
@@ -0,0 +1,15 @@
+package test;
+
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+        System.out.println("hello git");
+
+
+    }
+}
